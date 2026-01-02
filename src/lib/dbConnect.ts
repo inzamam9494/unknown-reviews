1033a6eac54f0847face8e8c9234b48fdb8a96d4
mohd inzamam ahmad <mohdinzamam12125@gmail.com>
2026-01-02 12:40:37 +0530

    create a db function for db connect

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,29 @@
+import moongoose from 'mongoose';
+
+type ConnectionObject = {
+    isConnection?: number;
+}
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+    if (connection.isConnection) {
+        console.log('Already connected to the database.');
+        return;
+    }
+
+    try {
+        const db = await moongoose.connect(process.env.MONGODB_URI || '', {})
+        connection.isConnection = db.connections[0].readyState;
+
+        console.log('DB connected successfully');
+        console.log(connection.isConnection);
+        console.log(db);
+        
+    } catch (error) {
+        console.log('DB connection failed:', error);
+        process.exit(1);
+    }
+}
+
+export default dbConnect;
