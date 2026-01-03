2c58e4aa4eccb46a80389db0831942529387762b
inzamam9494 <mohdraj537@gmail.com>
2026-01-03 15:09:35 +0530

    feat: add user sign-up API and email verification functionality
    - Implemented sign-up route to handle user registration.
    - Created helper function to send verification emails using Resend service.
    - Added Resend configuration for sending emails.
    - Defined ApiResponse type for consistent API responses.
    - Updated package.json to include necessary dependencies for bcrypt and email handling.

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,21 @@
+import dbConnect from "@/src/lib/dbConnect";
+import UserModel from "@/src/models/user.model";
+import bcrypt from "bcrypt";
+import { sendVerficationEmail } from "@/src/helper/sendVerficationEmail";
+import { success } from "zod";
+
+export async function POST(request: Request) {
+    await dbConnect();
+    try {
+        // const {username, email, password} = await request.json
+    } catch (error) {
+        console.error("Error in sign-up route:", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error Registering User",
+            },
+            { status: 500 }
+        );
+    }
+}
